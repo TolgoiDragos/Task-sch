5d0ea25cb5e50c106f8d662c61ea4747aec1234e
TolgoiDragos <39918655+TolgoiDragos@users.noreply.github.com>
2018-06-04 20:33:03 +0300

    attempt no2

---
+++ function.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct    //structuri
+{
+    int start;
+    int finish;
+    int duration;
+}task;
+
+void bubbleSort( task arr[], int n);
+int sortare(task taskuri[],int n);
+
+#endif
+
